e034cf308de5cbfae014d020f5f3cf3066c646d9
Mahmoud Hashemi <mahmoud@hatnote.com>
2015-06-02 01:12:55 -0700

    change app architecture to have a UI update schedule and fading, etc.

--- l2w_app/client.py
+++ l2w_app/client.py
@@ -1,11 +1,13 @@
 
 import sys
 import json
+import time
 
 #install_twisted_rector must be called before importing the reactor
 from kivy.support import install_twisted_reactor
 install_twisted_reactor()
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
@@ -17,6 +19,11 @@ from twisted.internet import reactor
 from twisted.internet.protocol import ReconnectingClientFactory
 from autobahn.twisted.websocket import (WebSocketClientFactory,
                                         WebSocketClientProtocol)
+
+MIN_SIZE = 3
+SCALE_FACTOR = 5
+START_OPACITY = 1.0
+FADEOUT_SECONDS = 15.0
 
 
 class L2WProtocol(WebSocketClientProtocol):
@@ -54,10 +61,43 @@ class L2WFactory(WebSocketClientFactory, ReconnectingClientFactory):
         self.retry(connector)
 
 
+class ChangeItem(object):
+    def __init__(self, metadata, app):
+        self.create_time = time.time()
+        self._app = app
+        self.metadata = metadata
+        self._set_color()
+        self._set_radius()
+        self._set_position()
+
+    def _set_color(self):
+        self.rgb = (1.0, 1.0, 1.0)
+
+    def _set_radius(self):
+        size = self.metadata['change_size']
+        self.radius = max((abs(size or 0) ** 0.5) * SCALE_FACTOR, MIN_SIZE)
+
+    def _set_position(self):
+        import hashlib
+        width, height = self._app.layout.width, self._app.layout.height
+        title = self.metadata['page_title'].encode('utf8')
+        digest = hashlib.md5(title).hexdigest()
+        xdigest, ydigest = digest[:8], digest[-8:]
+        x, y = int(xdigest, 16) % width, int(ydigest, 16) % height
+        self.pos = (x, y)
+
+    def __repr__(self):
+        cn = self.__class__.__name__
+        return ('<%s title=%r user=%r size=%r>'
+                % (cn, self.metadata['page_title'],
+                   self.metadata['user'], self.metadata['change_size']))
+
+
 class L2WApp(App):
     connection = None
 
     def build(self):
+        self.changes = []
         root = self.setup_gui()
         self.connect_to_server()
         return root
@@ -68,13 +108,15 @@ class L2WApp(App):
         self.layout = BoxLayout(orientation='vertical')
         self.layout.add_widget(self.label)
         self.layout.add_widget(self.textbox)
+
+        Clock.schedule_interval(self.update_ui, 1.0 / 60.0)
+
         return self.layout
 
     def connect_to_server(self):
         log.startLogging(sys.stdout)
 
         factory = L2WFactory(self, "ws://listen.hatnote.com:9000", debug=False)
-        factory.protocol = L2WProtocol
 
         reactor.connectTCP("listen.hatnote.com", 9000, factory)
         # reactor.run()
@@ -83,23 +125,53 @@ class L2WApp(App):
         self.print_message("connected succesfully!")
         self.connection = connection
 
-    def _msg_to_dimcoord(self, page_title, height, width):
-        import hashlib
-        # TODO: calc size, subtract from height/width to keep circle inside
-        digest = hashlib.md5(page_title).hexdigest()
-        xdigest, ydigest = digest[:16], digest[16:]
-        x, y = int(xdigest, 16) % height, int(ydigest, 16) % width
-        return (x, y), 100
-
     def handle_message(self, msg):
-        widget = self.layout
-        with widget.canvas:
-            Color(1.0, 1.0, 1.0, 0.1)
-            coord, size = self._msg_to_dimcoord(msg['page_title'],
-                                                widget.height,
-                                                widget.width)
-            Ellipse(pos=coord, size=(size, size))
-        self.label.text = str(msg)
+        change_item = ChangeItem(msg, app=self)
+        self.changes.append(change_item)
+        self.label.text = str(msg).encode('utf8')
+
+    def update_ui(self, dt):
+        layout = self.layout
+        layout.canvas.clear()
+        with layout.canvas:
+            cur_time = time.time()
+            next_changes = []
+            for change in self.changes:
+                fade = ((cur_time - change.create_time) / FADEOUT_SECONDS)
+                opacity = START_OPACITY - fade
+                if opacity <= 0:
+                    print 'removed change', change
+                    continue
+                next_changes.append(change)
+                color = change.rgb + (opacity,)
+                Color(*color)
+                Ellipse(pos=change.pos, size=(change.radius, change.radius))
+            self.changes[:] = next_changes
+        return
+
 
 if __name__ == '__main__':
     L2WApp().run()
+
+
+"""
+        import hashlib
+        page_title = msg['page_title'].encode('utf8')
+        change_size = msg['change_size']
+        digest = hashlib.md5(page_title).hexdigest()
+        xdigest, ydigest = digest[:8], digest[-8:]
+        x, y = int(xdigest, 16) % width, int(ydigest, 16) % height
+
+"""
+
+
+"""
+        import zlib
+        title = msg['page_title'].encode('utf8')
+        change_size = msg['change_size']
+        odd_chars = ''.join([c for i, c in enumerate(title) if i % 2 == 1])
+        even_chars = ''.join([c for i, c in enumerate(title) if i % 2 == 0])
+        xdigest, ydigest = zlib.adler32(even_chars), zlib.adler32(odd_chars)
+        x, y = xdigest % width, ydigest % height
+
+"""
